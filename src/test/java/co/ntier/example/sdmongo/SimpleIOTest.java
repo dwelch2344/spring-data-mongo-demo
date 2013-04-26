0568843b47d40bec0e5826d43f576ccc58639d79
David Welch <david@davidwelch.co>
2013-04-26 14:01:23 -0600

    cleanup + removed debug failure

--- src/test/java/co/ntier/example/sdmongo/SimpleIOTest.java
+++ src/test/java/co/ntier/example/sdmongo/SimpleIOTest.java
@@ -12,21 +12,14 @@ public class SimpleIOTest {
 
 	@Test(expected=IllegalArgumentException.class)
 	public void testScannerParam() {
-		SimpleIO io = new SimpleIO(null, System.out);
+		new SimpleIO(null, System.out);
 		fail("Should not accept null scanner");
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testPrintStreamParam() {
-		SimpleIO io = new SimpleIO(new Scanner(System.in), null);
+		new SimpleIO(new Scanner(System.in), null);
 		fail("Should not accept null PrintStream");
 	}
-	
-	@Test
-	public void testFailingCI(){
-		fail("This is just for debug");
-	}
-	
-	
 
 }
